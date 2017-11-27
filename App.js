95646d5f4e9cb33aba6036413ba5d24539d09e5d
Carlos Barragan <starttheshooting@hotmail.com>
2017-11-27 17:07:08 -0600

    project structure and header component

--- App.js
+++ App.js
@@ -1,21 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header/Header'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your appppp!</Text>
-      </View>
+        <Header title="Albums"/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
