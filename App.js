592cace708df086dc3842d4697abc02a20ea6acf
Carlos Barragan <starttheshooting@hotmail.com>
2017-11-29 13:31:37 -0600

    AlbumList component

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './src/components/Header/Header'
+import Header from './src/components/Header/Header';
+import AlbumList from './src/components/AlbumList/AlbumList';
 
 export default class App extends React.Component {
   render() {
     return (
+      <View>
         <Header title="Albums"/>
+        <AlbumList />
+      </View>
     );
   }
 }
---
+++ src/components/AlbumList/AlbumList.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class AlbumList extends Component {
+
+    render() {
+        return (
+            <View>
+                <Text>AlbumList Component</Text>
+            </View>
+        );
+    }
+
+} 
+
+export default AlbumList;
