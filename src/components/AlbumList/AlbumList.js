d5499b1bac8a8a90290f93843c7bd08b91238994
Carlos Barragan <starttheshooting@hotmail.com>
2017-11-29 22:49:51 -0600

    Card and AlbumDetails component

--- src/components/AlbumList/AlbumList.js
+++ src/components/AlbumList/AlbumList.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import axios from 'axios';
+import AlbumDetails from '../AlbumDetails/AlbumDetails';
 
 class AlbumList extends Component {
 
+    state = {
+        albums: []
+    };
+
     componentWillMount() {
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
-        .then( response => console.log(response));
+        .then(response => this.setState({ albums: response.data }));
+    }
+
+    renderAlbumDetails() {
+        return this.state.albums.map(album => {
+             return <AlbumDetails key={ album.title } album={ album }/>
+        });
     }
 
     render() {
         return (
             <View>
-                <Text>AlbumList Component</Text>
+                { this.renderAlbumDetails() }
             </View>
         );
     }
