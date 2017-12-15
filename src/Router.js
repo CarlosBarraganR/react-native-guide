a0ade20bd279ed182686b42628b9b3c6d29d2249
Carlos Barragan <starttheshooting@hotmail.com>
2017-12-15 16:03:14 -0600

    New Router Scene Flow

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,26 @@
 import React from 'react';
-import { Scene, Router } from 'react-native-router-flux';
+import { Scene, Router, Actions } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
 import EmployeeList from './components/EmployeesList';
+import EmployeeCreate from './components/EmployeeCreate';
 
 const RouterComponent = () => {
     return (
         <Router>
-            <Scene key="root">
-                <Scene key="login" component={LoginForm} title="Login" />
-                <Scene key="employeeList" component={EmployeeList} title="Employees" />
+            <Scene key="root" hideNavBar>
+                <Scene key="auth">
+                    <Scene initial key="login" component={LoginForm} title="Login" />
+                </Scene>
+                
+                <Scene key="main">
+                    <Scene 
+                        initial
+                        key="employeeList" 
+                        component={EmployeeList} 
+                        title="Employees" rightTitle="Add" 
+                        onRight={() => Actions.employeeCreate() } />
+                    <Scene key="employeeCreate" component={EmployeeCreate} title="Create Employee" />
+                </Scene>
             </Scene>
         </Router>
     );
---
+++ src/components/EmployeeCreate.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class EmployeeCreate extends Component {
+    render(){
+        return(
+            <View>
+                <Text>EMPLOYEE CREATE YAY!</Text>
+            </View>
+        );
+    }
+}
+
+export default EmployeeCreate;
